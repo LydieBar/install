c6ad7b2f45c569e4afbaf3fbc681a7ebb9637e09
lydie <lydie.barillot@epfedu.fr>
2023-11-29 15:52:03 +0100

    install_1

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update
+dpkg --configure -a
+apt upgrade -y
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/uzyexe/javascript-tetris.git
+cd javascript-tetris
+docker buildx build -t image-tetris .
+docker-compose up -d
