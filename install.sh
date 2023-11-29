b76993183a79c992ef6162b1221bb10942863cd3
lydie <lydie.barillot@epfedu.fr>
2023-11-29 16:53:24 +0100

    install_2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update
 dpkg --configure -a
-apt upgrade -y
+apt upgrade
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
