4e2195283511c31d785772309822f60117520aa4
lydie <lydie.barillot@epfedu.fr>
2023-12-06 09:54:28 +0100

    install final

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 apt update -y
 dpkg --configure -a
-apt upgrade -y
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu lsb_release -cs test"
+apt update
+apt install docker-ce -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/uzyexe/javascript-tetris.git
